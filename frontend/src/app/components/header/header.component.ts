0621b00a50a557be7f2e6fb3b8ad076bbc3dea33
JACE <jacexin77@gmail.com>
2025-05-27 14:21:50 +1200

    feat: added header

---
+++ frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatIconModule, MatButtonModule, MatMenuModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
